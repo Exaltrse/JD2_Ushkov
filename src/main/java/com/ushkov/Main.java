9f318b355c758cffc153ee91b15d4da2dc85fb42
Yegor Ushkov <exaltrse@gmail.com>
2021-04-08 14:02:19 +0300

    Commit for cherry-pick

--- src/main/java/com/ushkov/Main.java
+++ src/main/java/com/ushkov/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //First commit
         System.out.println("Hello");
-        //Second commit
+        //Commit for cherry-pick
     }
 }
